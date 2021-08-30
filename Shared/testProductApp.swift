a333b2c2011080dc4cfeafdc60f566fbcb5d6fff
Erva Hatun Tekeoğlu <erva@192.168.1.35>
2021-08-30 13:16:51 +0300

    initial commit

---
+++ Shared/testProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProductApp.swift
+//  Shared
+//
+//  Created by Erva Hatun Tekeoğlu on 30.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct testProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
